c5797519dc3f9470e7e5f2f7cafe17c57e18a98b
Austin Kelleher <a@alk.im>
2014-05-21 09:59:15 -0400

    Add additional safety checks and file types.
    Added a check to see if the path exists before using it, added new file types, expanded array for readability, made file types and working directory appear more constant.

--- filefinder.py
+++ filefinder.py
@@ -2,35 +2,58 @@ __author__ = 'thomas, steve'
 
 import os
 import fnmatch
-# Any kind of file you are looking for
-fileTypes = ["png", "jpg", "doc", " docx", "psd", "mp3", "mp4", "m4a", "pdf", "mov"]
-rootdir = os.getcwd
+
 #The root folder of where youd like to look
-workingdir = "/home/thomas/folder"
+WORKING_DIR = "/home/thomas/folder"
+
+# Any kind of file you are looking for
+FILE_TYPES = [
+    "png", 
+    "jpg", 
+    "jpeg", 
+    "gif", 
+    "doc", 
+    "docx", 
+    "psd", 
+    "mp3", 
+    "mp4", 
+    "m4a", 
+    "pdf", 
+    "mov", 
+    "zip", 
+    "rar", 
+    "csv", 
+    "ppt", 
+    "wav", 
+    "xls", 
+    "qt", 
+    "avi"
+]
+
+rootdir = os.getcwd
 
 def file_move(tobemoved, typeof):
     os.system("cp %s %s" % (tobemoved, (rootdir + "/RESTORED/" + typeof)))
-
 
 def find_files():
     currentdir = os.getcwd
     print("Working in" + currentdir + "\n")
 
-    for rootdir, dirdir, filedir in os.walk(workingdir):
+    for rootdir, dirdir, filedir in os.walk(WORKING_DIR):
         for folder in dirdir:
             print(folder + "\n")
-            if folder == "RESTORED" or folder in fileTypes:
+            if folder == "RESTORED" or folder in FILE_TYPES:
                 print("Skipping folders: " + folder + "\n")
             else:
                 print("Moving to %s \n" % folder)
                 os.chdir(folder)
-                for typeof in fileTypes:
+                for typeof in FILE_TYPES:
                     pat = "*." + typeof
                     for root, dirs, files in os.walk("."):
                         for name in files:
                             if fnmatch.fnmatch(name, pat):
-                                print(workingdir + folder + "/" + name)
-                                filepath = workingdir + folder + "/" + name
+                                print(WORKING_DIR + folder + "/" + name)
+                                filepath = WORKING_DIR + folder + "/" + name
                                 file_move(filepath, typeof)
                             else:
                                 print("Nothing found")
@@ -44,16 +67,18 @@ def premade_dir():
     os.system("test -d RESTORED; if( test $? -eq 1) then mkdir RESTORED; fi")
     os.chdir("RESTORED")
 
-    for extension in fileTypes:
+    for extension in FILE_TYPES:
         os.system("test -d %s; if( test $? -eq 1) then mkdir %s; fi" % (extension, extension))
         print(" " + extension + " ")
 
 
 def main():
-    print("Starting up \n")
-    premade_dir()
-    os.chdir(workingdir)
-    find_files()
+    if os.path.isdir(WORKING_DIR):
+        premade_dir()
+        os.chdir(WORKING_DIR)
+        find_files()
+    else:
+        print 'Directory: ' + WORKING_DIR + ' does not exist.'
 
 if __name__ == "__main__":
     main()
